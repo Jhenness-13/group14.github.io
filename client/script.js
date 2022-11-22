af18ff3d5037c7bdeec72ef5462323da0cc2410a
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2022-11-22 14:03:38 -0500

    done

--- client/script.js
+++ client/script.js
@@ -3,24 +3,17 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
 function processRequest(data) {
   const heights = [];
   const weights = [];
   const weightsHeights = [];
   data.forEach((element) => heights.push(element.height.meters));
   data.forEach((element) => weights.push(element.weight.kilograms));
-  // console.log(heights, weights);
   for (let i = 0; i < weights.length; i++) {
     weightsHeights.push([heights[i], weights[i]]);
   }
-  console.log(weightsHeights);
   return weightsHeights;
 }
-
 async function shapeDataForLineChart(array) {
   return array.reduce((collection, item) => {
     if (!collection[item.category]) {
@@ -31,7 +24,6 @@ async function shapeDataForLineChart(array) {
     return collection;
   }, {});
 }
-
 async function getData() {
   const url = 'https://api-nba-v1.p.rapidapi.com/players?team=1&season=2021';
   // const url = 'https://api-nba-v1.p.rapidapi.com/teams/statistics?id=2&season=2020';
@@ -65,4 +57,5 @@ async function mainEvent() {
       It's calling the 'mainEvent' function at line 57
       It runs first because the listener is set to when your HTML content has loaded
     */
+
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
